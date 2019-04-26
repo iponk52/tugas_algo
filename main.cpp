c1cad8d559645c87df78610f8e38351c2e5276c3
iponk52 <irfan.dharmawan.52@gmail.com>
2019-04-26 13:33:03 +0700

    clear

--- main.cpp
+++ main.cpp
@@ -74,6 +74,7 @@ int main(int argc, char** argv) {
 								cin>>c_op;
 								switch (c_op){
 									case '1':
+//kecepatan start									
 										system ("CLS");
 										cout<<"Masukan Jarak Tempuh Dengan Satuan KM : ";
 										cin>>a1;
@@ -97,6 +98,7 @@ int main(int argc, char** argv) {
 												cin>>op_menu;
 													if (op_menu == 'y'||op_menu == 'Y'){
 														system("CLS");
+														j1:
 														cout<<"untuk rumus mecari kecepatan adalah Jarak Di Bagi Waktu (v=s/t)\n";
 														cout<<"\n";
 														cout<< rumus;
@@ -137,7 +139,7 @@ int main(int argc, char** argv) {
 														}else{
 														system("CLS");
 														cout<<"kode yang anda masukan salah \n";
-														goto jawab_1;
+														goto j1;
 														}
 														
 													}else if (op_menu == 'm'||op_menu == 'M'){
@@ -157,7 +159,7 @@ int main(int argc, char** argv) {
 											cout<<"masukan waktu tempuh dengan satuan Jam = ";
 											cin>>aj;
 											system ("CLS");
-											jawab_1j :
+											jawab_2j :
 											cout<<"**************************************************************************************************************\n";
 											cout<<"*                                                                                                            *\n";
 											cout<<" kecepatan dari jarak tempuh "<<a1<<" KM"<<" dengan waktu "<<aj<<" jam"<<" adalah "<<a1/aj<<"KM/Jam     \n";
@@ -169,6 +171,7 @@ int main(int argc, char** argv) {
 												cin>>op_menu;
 													if (op_menu == 'y'||op_menu == 'Y'){
 														system("CLS");
+														j2:
 														cout<<"untuk rumus mecari kecepatan adalah Jarak Di Bagi Waktu (v=s/t)\n";
 														cout<<"\n";
 														cout<< rumus;
@@ -189,6 +192,160 @@ int main(int argc, char** argv) {
 																<<"jadi kecepatan dari jarak tempuh "<<a1
 																<<" KM"<<" dengan waktu "<<aj<<" jam adalah "
 																<<a1/aj<<" KM/Jam"
+																<<"\n"
+																<<"\n";
+														cout<<"(M) = Kembali Ke Menu Utama      (X) = Keluar";
+														cout<<"\n"
+															<<"\n"
+															<<"silahkan masukan pilihan anda = ";
+														cin>>op_menu;
+														if (op_menu == 'm'||op_menu == 'M'){
+														system("CLS");
+														goto menu;
+														}else if (op_menu == 'x'|| op_menu == 'X'){
+															system("CLS");
+															cout<<bye;
+															break ;
+															exit(0);
+														}else{
+														system("CLS");
+														cout<<"kode yang anda masukan salah \n";
+														goto j2;
+														}
+														
+													}else if (op_menu == 'm'||op_menu == 'M'){
+														system("CLS");
+														goto menu;
+													}else if (op_menu == 'x'|| op_menu == 'X'){
+														system("CLS");
+														cout<<bye;
+														break ;
+														exit(0);
+													}else{
+													system("CLS");
+													cout<<"kode yang anda masukan salah \n";
+													goto j2;
+													};
+										}else{
+											system ("CLS");
+											cout<<"pilihan yang anda masukan salah \n";
+											goto waktu;
+										};
+										
+										break;
+//kecepatan end									
+									case '2':
+//jarak tempuh start				
+										system ("CLS");
+										cout<<"Masukan Kecepatan Dengan Satuan KM/Jam : ";
+										cin>>a1;
+										cout<< "pilih satuan waktu, (J)=jam atau (M)=Menit = ";
+										cin>>satuan;
+										if (satuan == 'm'|| satuan == 'M'){
+											cout<<"masukan waktu tempuh dengan satuan Menit = ";
+											cin>>am;
+											hasil = am/60;
+											system ("CLS");
+											jawab_3 :
+											cout<<"**************************************************************************************************************\n";
+											cout<<"*                                                                                                            *\n";
+											cout<<"      Jarak tempuh dari kecepatan "<<a1<<" KM/jam"<<" dengan waktu "<<am<<" menit adalah "<<a1/hasil<<" KM     \n";
+											cout<<"*                                                                                                            *\n";
+											cout<<"*   (Y) = melihat menjabaran     (M) = Kembali Ke Menu Utama      (X) = Keluar                               *\n";
+											cout<<"**************************************************************************************************************\n";
+											cout<<"\n";
+											cout<<"ingin melihat menjabarannya ?, pilih 'Y' untuk melihat, atau 'M' untuk kembali ke menu = " ;
+												cin>>op_menu;
+													if (op_menu == 'y'||op_menu == 'Y'){
+														system("CLS");
+														cout<<"untuk rumus mecari jarak adalah Kecepatan Di Kali Waktu (s = v * t)\n";
+														cout<<"\n";
+														cout<< rumus;
+														cout<<"\n";
+														cout<< jabar_1<<" "<<a1<<" * ("<< am << "/60 )\n";
+														cout<<"\n";
+														cout<<"karena waktu yang di gunakan adalah menit, maka di ubah menjadi jam dengan cara (menit / 60)\n";
+														cout<<"\n";
+														cout<<"jadi "<<am<<"/ 60"<<" = "<<hasil<<" jam\n";
+														cout<<"\n";
+														cout<<	"dik : \n"
+																<<"v = "<<a1<<" KM/jam\n"
+																<<"t = "<<hasil<<" jam\n"
+																<<"dit : \n"
+																<<"s   = ?\n"
+																<<"s   = "<<a1<<" * "<<hasil<<"\n"
+																<<"    = "<<a1/hasil<<" KM"
+																<<"\n"
+																<<"\n"
+																<<"jadi jarak dari kecepatan "<<a1
+																<<" KM/jam"<<" dengan waktu "<<am<<" menit adalah "
+																<<a1/hasil<<" KM/Jam"
+																<<"\n"
+																<<"\n";
+														cout<<"(M) = Kembali Ke Menu Utama      (X) = Keluar";
+														cout<<"\n"
+															<<"\n"
+															<<"silahkan masukan pilihan anda = ";
+														cin>>op_menu;
+														if (op_menu == 'm'||op_menu == 'M'){
+														system("CLS");
+														goto menu;
+														}else if (op_menu == 'x'|| op_menu == 'X'){
+															system("CLS");
+															cout<<bye;
+															break ;
+															exit(0);
+														}else{
+														system("CLS");
+														cout<<"kode yang anda masukan salah \n";
+														goto jawab_3;
+														}
+														
+													}else if (op_menu == 'm'||op_menu == 'M'){
+														system("CLS");
+														goto menu;
+													}else if (op_menu == 'x'|| op_menu == 'X'){
+														system("CLS");
+														cout<<bye;
+														break ;
+														exit(0);
+													}else{
+													system("CLS");
+													cout<<"kode yang anda masukan salah \n";
+													goto jawab_3;
+													}
+										}else if (satuan == 'J' || satuan == 'j'){
+											cout<<"masukan waktu tempuh dengan satuan Jam = ";
+											cin>>aj;
+											system ("CLS");
+											jawab_1j :
+											cout<<"**************************************************************************************************************\n";
+											cout<<"*                                                                                                            *\n";
+											cout<<"      Jarak tempuh dari kecepatan "<<a1<<" KM/jam"<<" dengan waktu "<<am<<" menit adalah "<<a1*aj<<" KM     \n";
+											cout<<"*                                                                                                            *\n";
+											cout<<"*   (Y) = melihat menjabaran     (M) = Kembali Ke Menu Utama      (X) = Keluar                               *\n";
+											cout<<"**************************************************************************************************************\n";
+											cout<<"\n";
+											cout<<"ingin melihat menjabarannya ?, pilih 'Y' untuk melihat, atau 'M' untuk kembali ke menu = " ;
+												cin>>op_menu;
+													if (op_menu == 'y'||op_menu == 'Y'){
+														system("CLS");
+														cout<<"untuk rumus mecari jarak adalah Kecepatan Di Kali Waktu (s = v * t)\n";
+														cout<<"\n";
+														cout<< aj;
+														cout<<"\n";
+														cout<<	"dik : \n"
+																<<"v = "<<a1<<" KM/jam\n"
+																<<"t = "<<aj<<" jam\n"
+																<<"dit : \n"
+																<<"s   = ?\n"
+																<<"s   = "<<a1<<" * "<<aj<<"\n"
+																<<"    = "<<a1*aj<<" KM"
+																<<"\n"
+																<<"\n"
+																<<"jadi jarak dari kecepatan "<<a1
+																<<" KM/jam"<<" dengan waktu "<<am<<" menit adalah "
+																<<a1*aj<<" KM"
 																<<"\n"
 																<<"\n";
 														cout<<"(M) = Kembali Ke Menu Utama      (X) = Keluar";
@@ -230,10 +387,83 @@ int main(int argc, char** argv) {
 										};
 										
 										break;
-									case '2':
-										printf ("Jarak");
+//jarak tempuh end										
 									case '3':
-										printf ("waktu");
+//waktu tempuh
+										system ("CLS");
+										cout<<"Masukan Jarak Tempuh Dengan Satuan KM : ";
+										cin>>a1;
+										cout<< "Masukan Kecepatan Tempuh Dengan Satuan KM/Jam : ";
+										cin>>aj;
+										system ("CLS");
+										jawab_4j :
+										cout<<"**************************************************************************************************************\n";
+										cout<<"*                                                                                                            *\n";
+										cout<<"      Waktu tempuh dari Jarak "<<a1<<" KM"<<" dengan kecepatan "<<a1<<" KM/Jam adalah "<<a1/aj<<" jam     \n";
+										cout<<"*                                                                                                            *\n";
+										cout<<"*   (Y) = melihat menjabaran     (M) = Kembali Ke Menu Utama      (X) = Keluar                               *\n";
+										cout<<"**************************************************************************************************************\n";
+										cout<<"\n";
+										cout<<"ingin melihat menjabarannya ?, pilih 'Y' untuk melihat, atau 'M' untuk kembali ke menu = " ;
+											cin>>op_menu;
+												if (op_menu == 'y'||op_menu == 'Y'){
+													system("CLS");
+													cout<<"untuk rumus mecari waktu tempuh adalah Jarak di bagi kecepatan (t = s / v)\n";
+													cout<<"\n";
+													cout<< rumus;
+													cout<<"\n";
+													cout<< jabar_1<<" "<<a1<<" / ("<< am << "/60 )\n";
+													cout<<"\n";
+													cout<<"karena waktu yang di gunakan adalah menit, maka di ubah menjadi jam dengan cara (menit / 60)\n";
+													cout<<"\n";
+													cout<<"jadi "<<am<<"/ 60"<<" = "<<aj<<" jam\n";
+													cout<<"\n";
+													cout<<	"dik : \n"
+															<<"s = "<<a1<<" KM\n"
+															<<"v = "<<aj<<" KM/jam\n"
+															<<"dit : \n"
+															<<"t   = ?\n"
+															<<"t   = "<<a1<<" / "<<aj<<"\n"
+															<<"    = "<<a1/aj<<" Jam"
+															<<"\n"
+															<<"\n"
+															<<"jadi waktu dari jarak "<<a1
+															<<" KM"<<" dengan dengan kecepatan "<<am<<" KM/Jam adalah "
+															<<a1/aj<<" Jam"
+															<<"\n"
+															<<"\n";
+													cout<<"(M) = Kembali Ke Menu Utama      (X) = Keluar";
+													cout<<"\n"
+														<<"\n"
+														<<"silahkan masukan pilihan anda = ";
+													cin>>op_menu;
+														if (op_menu == 'm'||op_menu == 'M'){
+														system("CLS");
+														goto menu;
+														}else if (op_menu == 'x'|| op_menu == 'X'){
+															system("CLS");
+															cout<<bye;
+															break ;
+															exit(0);
+														}else{
+														system("CLS");
+														cout<<"kode yang anda masukan salah \n";
+														goto jawab_4j;
+														}
+														
+													}else if (op_menu == 'm'||op_menu == 'M'){
+														system("CLS");
+														goto menu;
+													}else if (op_menu == 'x'|| op_menu == 'X'){
+														system("CLS");
+														cout<<bye;
+														break ;
+														exit(0);
+													}else{
+													system("CLS");
+													cout<<"kode yang anda masukan salah \n";
+													goto jawab_1;
+													};
 									case 'm' :
 										system ("CLS");
 										goto menu;
